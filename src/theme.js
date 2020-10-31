7c19d83fb55c8e921b68cbb8580b6ae2aeee65df
James Barron <jamesbarron97@gmail.com>
2020-10-31 17:14:06 +0000

    Wrapping in theme, setting up fonts

---
+++ src/theme.js
@@ -0,0 +1,9 @@
+const { createMuiTheme } = require("@material-ui/core");
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: '"Nanum Gothic", "Roboto", "Arial", sans-serif',
+  },
+});
+
+export default theme;
