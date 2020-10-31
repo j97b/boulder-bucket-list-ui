391b990d90c9fdd63e977d24ed9d5ff533ea5410
James Barron <jamesbarron97@gmail.com>
2020-10-31 17:08:46 +0000

    Setting up w/ material-ui

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+const App = () => {
+    return (
+        <Typography>
+            Under Construction
+        </Typography>
+    );
+};
+
+export default App;
