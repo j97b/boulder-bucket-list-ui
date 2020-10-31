d100b55e9b5a7c7089a946c7c4677abd01d0b856
James Barron <jamesbarron97@gmail.com>
2020-10-31 18:41:43 +0000

    wrapping buttons in link tags

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { AppBar, Button, IconButton, Toolbar, Typography } from '@material-ui/core';
 import styles from './styles';
 import { AccountCircleRounded } from '@material-ui/icons';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     const classes = styles();
@@ -11,10 +12,18 @@ const Navbar = () => {
         <div className={classes.root}>
             <AppBar>
                 <Toolbar>
-                    <Typography variant='h6' className={classes.title}>Boulder Bucket List</Typography>
-                    <Button color='inherit' className={classes.button}>Bucket List</Button>
-                    <Button color='inherit' className={classes.button}>Map</Button>
+                    <Link to='/'>
+                        <Typography variant='h6' className={classes.title}>Boulder Bucket List</Typography>
+                    </Link>
+                    <Link to='/bucketList'>
+                        <Button color='inherit' className={classes.button}>Bucket List</Button>
+                    </Link>
+                    <Link to='/map'>
+                        <Button color='inherit' className={classes.button}>Map</Button>
+                    </Link>
+                    <Link to='/addBoulder'>
                     <Button color='inherit' className={classes.button}>Add Boulder</Button>
+                    </Link>
                     <IconButton color='inherit'>
                         <AccountCircleRounded /> 
                     </IconButton>
