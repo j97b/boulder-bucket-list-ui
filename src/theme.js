5dd05e68d35a9c8a7fd3a1a2b2b909e0db490928
James Barron <jamesbarron97@gmail.com>
2020-10-31 18:32:59 +0000

    changing colour palette

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,11 @@
 const { createMuiTheme } = require("@material-ui/core");
 
 const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#788B91",
+    },
+  },
   typography: {
     fontFamily: '"Nanum Gothic", "Roboto", "Arial", sans-serif',
   },
