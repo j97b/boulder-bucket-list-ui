5cf9c88470d7b6bfe97348312d3e8e2dbc38208f
James Barron <jamesbarron97@gmail.com>
2020-11-01 20:27:12 +0000

    bucket list takes values from api

--- src/pages/bucketList/BucketList.jsx
+++ src/pages/bucketList/BucketList.jsx
@@ -1,24 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Typography } from "@material-ui/core";
+import axios from "axios";
 
 import styles from "./styles";
 import BoulderCard from "../../components/boulderCard/BoulderCard";
 
 const BucketList = () => {
+  const [boulders, setBoulders] = useState([]);
   const classes = styles();
+
+  useEffect(() => {
+    axios
+      .get("http://3.9.14.244:3000/boulders/")
+      .then((res) => setBoulders(res.data));
+  });
 
   return (
     <>
       <Typography variant="h2" className={classes.heading}>
         Bucket List
       </Typography>
-      <BoulderCard
-        name="Girl On Our Mind"
-        location="Rocklands (South Africa)"
-        grade="6C+"
-        completed="true"
-        completionDate="23/02/2019"
-      />
+      {boulders.map((boulder) => (
+        <BoulderCard
+          name={boulder.name}
+          location={boulder.locationName}
+          grade={boulder.grade}
+          completed={boulder.completed}
+          completionDate={boulder.completionDate}
+        />
+      ))}
     </>
   );
 };
